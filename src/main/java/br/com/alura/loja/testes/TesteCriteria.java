80ba7469ead8c376cf3a5b3e404fce277f446a36
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-02 14:30:56 -0300

    Consulta: consulta com parâmetros dinâmicos com Criteria API

---
+++ src/main/java/br/com/alura/loja/testes/TesteCriteria.java
@@ -0,0 +1,48 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ClienteDao;
+import br.com.alura.loja.dao.PedidoDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.model.*;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class TesteCriteria {
+    public static void main(String[] args) {
+        popularBancoDeDados();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        produtoDao.buscarPorParametrosComCriteria(null, null, LocalDate.now());
+    }
+
+    private static void popularBancoDeDados() {
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("3000"), videogames);
+        Produto macbook = new Produto("Macbook", "Macbook Pro", new BigDecimal("5000"), informatica);
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(entityManager);
+        CategoriaDao categoriaDao = new CategoriaDao(entityManager);
+
+        entityManager.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
+        produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videogame);
+        produtoDao.cadastrar(macbook);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
